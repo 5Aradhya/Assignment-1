298916f83288c357234d124ae53a85159cd88604
Aradhya Uttam <aradhyauttam15@gmail.com>
2024-05-27 19:01:34 +0530

    Add files via upload

---
+++ Ques10.c
@@ -0,0 +1,10 @@
+//Ques-10 Write a c program that calculates the volume of a sphere.
+
+#include<stdio.h>
+int main(){
+ float radius;
+ float volume;
+ radius=3.54;
+ volume=(1.3*3.14*3.54*3.54*3.54);
+ printf ("%f", volume);
+}
