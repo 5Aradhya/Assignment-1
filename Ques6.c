4f4355bb5401f92dad945c055546d098279de970
Aradhya Uttam <aradhyauttam15@gmail.com>
2024-05-27 18:43:04 +0530

    Add files via upload

---
+++ Ques6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    printf("enter first number");
+    scanf("%d", &a);
+    printf("enter second number");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("%d", sum);
+}
