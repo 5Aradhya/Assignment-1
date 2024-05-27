e9d3ae206e2d86d90c27b9bc5383b50bf97e661c
Aradhya Uttam <aradhyauttam15@gmail.com>
2024-05-27 15:52:33 +0530

    Add files via upload

---
+++ Ques2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    // Assisgnment-1 Ques-2 write a program to "Hello" in the first line and "KGI !!" in the second line 
+    printf("\n\"Hello\"\n");// First Line
+    printf("\n\"KGI\"\n"); //Second line 
+}
